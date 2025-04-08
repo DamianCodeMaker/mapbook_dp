85478f45dc93a1b18413d55352335b33e4dfa86c
DamianCodeMaker <u64789@student.wat.edu.pl>
2025-04-08 18:30:57 +0200

    wip: data structure

---
+++ main.py
@@ -0,0 +1,11 @@
+user = [
+    {"name": "Damian", "location": "Kraków", "posts": 555},
+    {"name": "Mikołaj", "location": "Przasnysz", "posts": 200},
+    {"name": "Krzysztof", "location": "Poznań", "posts": 100},
+    {"name": "Bartosz", "location": "Ostrołęka", "posts": 300},
+
+]
+
+# twój znajmowy krzysztof z opublikował tyle i tyle postów
+for user in users:
+    print(f"Twój znajomy {user['name']}, z {user['location']} opublikował {user['posts']} postów")
