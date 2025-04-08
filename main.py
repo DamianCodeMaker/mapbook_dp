3644a5fec3113dd8ace4710d7f3dc20b63a01f9a
DamianCodeMaker <u64789@student.wat.edu.pl>
2025-04-08 18:47:12 +0200

    wip: data structure, get users

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-user = [
+users = [
     {"name": "Damian", "location": "Kraków", "posts": 555},
     {"name": "Mikołaj", "location": "Przasnysz", "posts": 200},
     {"name": "Krzysztof", "location": "Poznań", "posts": 100},
@@ -6,5 +6,10 @@ user = [
 
 ]
 
-for user in users:
-    print(f"Twój znajomy {user['name']}, z {user['location']} opublikował {user['posts']} postów")
+
+def get_user_info(users_data: list) -> None:
+    for user in users_data:
+        print(f"Twój znajomy {user['name']}, z miejscowości: {user['location']} opublikował {user['posts']} postów")
+
+
+get_user_info(users)
